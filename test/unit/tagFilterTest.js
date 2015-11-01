82462f010625642a76042f44f1f3670f7fdf6ca4
evanRoberto <3_van@live.com>
2015-11-01 22:27:34 +0800

    test Cases
    just enough to pass the min required 50% branch coverage

--- test/unit/tagFilterTest.js
+++ test/unit/tagFilterTest.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var questionList=[{
+var tagList=[{
   wholeMsg: "newTodo",
   head: "head",
   headLastChar: "?",
@@ -8,8 +8,33 @@ var questionList=[{
   linkedDesc: "linkedDesc",
   completed: false,
   timestamp: 0,
-  tags: "...",
-  echo: 3,
+  tags: "aaa",
+  upvote: 0,
+  downvote: 0,
+  order: 1
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "bbb",
+  upvote: 0,
+  downvote: 0,
+  order: 2
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "ccc",
+  upvote: 0,
+  downvote: 0,
   order: 3
 },{
   wholeMsg: "newTodo",
@@ -19,8 +44,9 @@ var questionList=[{
   linkedDesc: "linkedDesc",
   completed: false,
   timestamp: 0,
-  tags: "...",
-  echo: 2,
+  tags: "ddd",
+  upvote: 0,
+  downvote: 0,
   order: 4
 },{
   wholeMsg: "newTodo",
@@ -29,9 +55,10 @@ var questionList=[{
   desc: "desc",
   linkedDesc: "linkedDesc",
   completed: false,
-  timestamp: 0,
-  tags: "...",
-  echo: 2,
+  timestamp: new Date().getTime(), //new
+  tags: "eee",
+  upvote: 0,
+  downvote: 0,
   order: 5
 },{
   wholeMsg: "newTodo",
@@ -40,9 +67,10 @@ var questionList=[{
   desc: "desc",
   linkedDesc: "linkedDesc",
   completed: false,
-  timestamp: 0,
-  tags: "...",
-  echo: 2,
+  timestamp: new Date().getTime()-1, //new
+  tags: "fff",
+  upvote: 3,
+  downvote: 2,
   order: 6
 },{
   wholeMsg: "newTodo",
@@ -51,57 +79,37 @@ var questionList=[{
   desc: "desc",
   linkedDesc: "linkedDesc",
   completed: false,
-  timestamp: new Date().getTime(), //new
-  tags: "...",
-  echo: 2,
-  order: 0
-},{
-  wholeMsg: "newTodo",
-  head: "head",
-  headLastChar: "?",
-  desc: "desc",
-  linkedDesc: "linkedDesc",
-  completed: false,
-  timestamp: new Date().getTime()-1, //new
-  tags: "...",
-  echo: 0,
-  order: 2
-},{
-  wholeMsg: "newTodo",
-  head: "head",
-  headLastChar: "?",
-  desc: "desc",
-  linkedDesc: "linkedDesc",
-  completed: false,
   timestamp: new Date().getTime(), // latest
-  tags: "...",
-  echo: 0,
-  order: 1
+  tags: "ggg",
+  upvote: 1,
+  downvote: 1,
+  order: 7
+//},
 }];
 
 describe('TodoCtrl', function() {
   beforeEach(module('todomvc'));
 
-  describe('questionFilter Testing', function() {
+  describe('tagFilter Testing', function() {
     beforeEach(module(function($provide) {
       $provide.value('version', 'TEST_VER'); //TODO: what is this provide?
       console.log("provide.value: " + $provide.value);
     }));
 
-    it('has a question filter', inject(function($filter) {
-      expect($filter('questionFilter')).not.toBeNull();
+    it('has a tag filter', inject(function($filter) {
+      expect($filter('tagFilter')).not.toBeNull();
     }));
 
-    it('Filter order test', inject(function(questionFilterFilter) { // need to put Filter suffix
-      var filteredList = questionFilterFilter(questionList, 100);
+    it('Filter order test', inject(function(tagFilterFilter) { // need to put Filter suffix
+      var filteredList = tagFilterFilter(tagList, 100);
       for (var i in filteredList) {
         expect(""+filteredList[i].order).toEqual(i);
       }
     }));
 
-    it('Filter max test', inject(function(questionFilterFilter) { // need to put Filter suffix
-      var filteredList = questionFilterFilter(questionList, 1);
-      expect(filteredList.length).toEqual(5);
+    it('Filter max test', inject(function(tagFilterFilter) { // need to put Filter suffix
+      var filteredList = tagFilterFilter(tagList, 1);
+      expect(filteredList.length).toEqual(0);
 
       for (var i in filteredList) {
         expect(""+filteredList[i].order).toEqual(i);
