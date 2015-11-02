ac60a7ea815b613897b5c0a71ca15b3760b02fef
Alan Yip <koyip@connect.ust.hk>
2015-11-02 15:02:41 +0800

    Fixed the order issue of old questions (> 3 minutes ago).

--- js/filters/questionFilter.js
+++ js/filters/questionFilter.js
@@ -31,6 +31,13 @@ todomvc.filter('questionFilter', function () {
       }
       return (b.upvote-b.downvote) - (a.upvote-a.downvote);
     });
+	
+    sorted.sort(function(a, b) {
+      if (a.upvote-a.downvote == b.upvote-b.downvote) {
+        return b.timestamp - a.timestamp;
+      }
+      return (b.upvote-b.downvote) - (a.upvote-a.downvote);
+    });
 
     // Combined list
     return newQuestions.concat(sorted);
